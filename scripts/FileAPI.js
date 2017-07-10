456750151af7973e0d979abdef4d96b8fc91e93e
CloseGillter47 <LiJQ2016@gmail.com>
2017-07-10 18:11:26 +0800

    FileAPI接口加入注释

--- scripts/FileAPI.js
+++ scripts/FileAPI.js
@@ -5,7 +5,11 @@ const PATH = require('path')
 const EXCEL = require('./ExcelAPI')
 
 
-
+/**
+ * 统一写入文件接口
+ * @param {string} path path 文件路径
+ * @returns {object} 返回读取的文件内容
+ */
 function read(path) {
 
     let ext = PATH.extname(path);
@@ -30,8 +34,39 @@ function read(path) {
 
 }
 
+/**
+ * 统一写入文件接口
+ * @param {string} path path 文件路径
+ * @param {array} data data 文件内容
+ * @param {object||array} [config] config 额为配置
+ * @returns {boolean} 返回操作的成功标志
+ */
 function write(path, data, config) {
 
+    let res = false;
+
+    let ext = PATH.extname(path);
+
+    if (ext === '.xlsx') {
+
+        res = EXCEL.write(path, data, config);
+
+        return res;
+
+    }
+
+    if (ext === '.json') {
+
+        let _data = JSON.stringify(data);
+
+        res = FS.writeFileSync(path, _data, 'utf8');
+
+        return res;
+
+    }
+
 }
+
+
 
 module.exports = { read, write }
