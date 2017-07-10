04dfedab30885e27346f733788a3dcf1ea474518
CloseGillter47 <LiJQ2016@gmail.com>
2017-07-10 18:13:03 +0800

    新增测试脚本
    1.测试文件读写；
    2.测试加密、解密；

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const ExcelAPI = require('./scripts/ExcelAPI')
 const FileAPI = require('./scripts/FileAPI')
+const $t = require('./scripts/Util')
 
 const path = require('path');
 
@@ -12,10 +13,33 @@ let res;
 
 // console.log(res);
 
-// /////////////////////////////////////////////////////////////////////////////////////
+// ---------------------------------------------------------------------------------- //
+
+// let data = [
+//     [['表头1', '表头2', '表头3', '表头4', '表头5'], ['1001', '1002', '1003', '1004', '1005'], ['2001', '2002', '2003', '2004', '2005']],
+//     [['表头A', '表头B', '表头C', '表头D', '表头E'], ['MOOA', 'MOOB', 'MOOC', 'MOOD', 'MOOE'], ['NOOA', 'NOOB', 'NOOC', 'NOOD', 'NOOE']]
+// ];
+
+// res = ExcelAPI.write(path.join(__dirname, 'scripts', 'db', 'export', 'out-test.xlsx'), data, ['第一学期', '第二学期']);
+
+// console.log(res);
 
 // /////////////////////////////////////////////////////////////////////////////////////
 
-res = FileAPI.read(path.join(__dirname, 'scripts', 'db', 'source', 'test1.xlsx'));
+
+// res = FileAPI.read(path.join(__dirname, 'scripts', 'db', 'source', 'test.xlsx'));
+
+// console.log(res);
+
+
+// /////////////////////////////////////////////////////////////////////////////////////
+
+res = $t.getEncAse192('admin', 'code');
 
 console.log(res);
+
+res = $t.getDecAse192(res, 'code');
+
+console.log(res);
+
+// /////////////////////////////////////////////////////////////////////////////////////
